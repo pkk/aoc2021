f696314b7d073e70b5fef7ea19c23d8313e1bbcf
KKP <krishnakamal@gmail.com>
2021-12-02 13:34:47 +0530

    Day 2 refactor

--- day2.py
+++ day2.py
@@ -3,19 +3,7 @@ def read_file():
         lines = f.readlines()
     return lines
 
-def day2_a(lines):
-    depth, height = 0, 0 
-    for line in lines:
-        arr = line.split()
-        if arr[0] == "up":
-            depth -= int(arr[1])
-        elif arr[0] == "down":
-            depth += int(arr[1])
-        else:
-            height += int(arr[1])
-    return depth * height
-
-def day2_b(lines):
+def day2(lines):
     d,h,a = 0,0,0
     for line in lines:
         arr = line.split()
@@ -26,9 +14,8 @@ def day2_b(lines):
         else:
             h += int(arr[1])
             d += a * int(arr[1])
-    return d * h
+    return h * a, d * h
             
 if __name__ == "__main__":
     lines = read_file()
-    print("Day 2 a:", day2_a(lines))
-    print("Day 2 b:", day2_b(lines))
+    print("Day 2 a, b:", day2(lines))
